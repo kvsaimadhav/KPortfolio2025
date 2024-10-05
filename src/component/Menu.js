ea27e40eee14695fdb05b32d9aa93fc9186542cd
Sai Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2024-10-06 04:46:33 +0530

    Update Menu.js v 4.6.2

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -14,19 +14,19 @@ function Menu()
   return(
     <div className="Menu">
      <ul className="menu">
-         <li><a href="#home" rel="no-opener">Home</a></li>
-         <li><a href="#work" rel="no-opener">Work</a></li>
-         <li><a href="#project" rel="no-opener">Project</a></li>
-         <li><a href="#academic" rel="no-opener">Academic</a></li>
-         <li><a href="#contact" rel="no-opener">Contact</a></li>
-         <li><a href="#" rel="no-opener">Documents</a>
+         <li><a href="#home" rel="no-referrer no-opener">Home</a></li>
+         <li><a href="#work" rel="no-referrer no-opener">Work</a></li>
+         <li><a href="#project" rel="no-referrer no-opener">Project</a></li>
+         <li><a href="#academic" rel="no-referrer no-opener">Academic</a></li>
+         <li><a href="#contact" rel="no-referrer no-opener">Contact</a></li>
+         <li><a href="#" rel="no-referrer no-opener">Documents</a>
             <ul class="doc">
-              <li><a href={resume} rel="no-opener" target="_blank">Resume</a></li>
-              <li><a href={cv} rel="no-opener" target="_blank">Curriculum Vitae</a></li>
-              <li><a href="#" rel="no-opener">Certifications</a>
+              <li><a href={resume} rel="no-referrer no-opener" target="_blank">Resume</a></li>
+              <li><a href={cv} rel="no-referrer no-opener" target="_blank">Curriculum Vitae</a></li>
+              <li><a href="#" rel="no-referrer no-opener">Certifications</a>
                 <ul class="cert">
-                  <li><a href={aws} rel="no-opener" target="_blank">Amazon Web Services</a></li>
-                  <li><a href={oracle} rel="no-opener" target="_blank">Oracle</a></li>
+                  <li><a href={aws} rel="no-referrer no-opener" target="_blank">Amazon Web Services</a></li>
+                  <li><a href={oracle} rel="nno-referrer o-opener" target="_blank">Oracle</a></li>
                 </ul>
               </li>
             </ul>
@@ -34,8 +34,8 @@ function Menu()
          <li>
             <a href="#" rel="no-opener">connect</a>
             <ul class="profile">
-              <li><a href={linkINURL} rel="no-opener" target="_blank"><img class="linkedin" src={linkedin} referrerPolicy="no-referrer"/>&nbsp;LinkedIn</a></li>
-              <li><a href={gitHBURL} rel="no-opener" target="_blank"><img class="github" src={github} referrerPolicy="no-referrer"/>&nbsp;GitHub</a></li>
+              <li><a href={linkINURL} rel="no-referrer no-opener" target="_blank"><img class="linkedin" src={linkedin} referrerPolicy="no-referrer"/>&nbsp;LinkedIn</a></li>
+              <li><a href={gitHBURL} rel="no-referrer no-opener" target="_blank"><img class="github" src={github} referrerPolicy="no-referrer"/>&nbsp;GitHub</a></li>
             </ul>
          </li>
      </ul>
