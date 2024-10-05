783858454d4295bf8399b8ea0d85515e6bcaefc4
Sai Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2024-10-06 04:41:57 +0530

    Update Home.js v4.6.0

--- src/component/Home.js
+++ src/component/Home.js
@@ -9,7 +9,7 @@ function Home()
       <table className="table">
        <tr>
         <td className="image">
-          <img src={Photo} alt="Self Photo"/>
+          <img src={Photo}/>
         </td>
         <td className="para">
           <p>
