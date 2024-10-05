f88224ec015a5d23f22e994afc0ea783e5898fa7
Sai Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2024-10-06 04:42:57 +0530

    Update Home.js v4.6.1

--- src/component/Home.js
+++ src/component/Home.js
@@ -15,8 +15,8 @@ function Home()
           <p>
             <b>Hi</b>, <i>Sai Madhav</i> .... <br/>
             <i>Software Engineer</i> with interest for <b>Web Development</b> .... <br/>
-            You can reach out to me via <a href="https://www.linkedin.com/in/smadhavkv96/" rel="no-opener" target="_blank">LinkedIn</a> 
-            &nbsp; <i>or</i> &nbsp; <a href="https://github.com/kvsaimadhav" rel="no-opener" target="_blank">Github</a>
+            You can reach out to me via <a href="https://www.linkedin.com/in/smadhavkv96/" rel="no-referrer no-opener" target="_blank">LinkedIn</a> 
+            &nbsp; <i>or</i> &nbsp; <a href="https://github.com/kvsaimadhav" rel="no-referrer no-opener" target="_blank">Github</a>
           </p>
         </td>
         </tr>
