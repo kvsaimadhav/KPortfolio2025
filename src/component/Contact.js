9bc5968ca50d76cf24932a6fec0ec0626fdadf05
Sai Madhav <65571082+kvsaimadhav@users.noreply.github.com>
2024-10-24 19:57:48 +0530

    Update Contact.js for version 3.0.1

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -27,19 +27,19 @@ function Contact()
                    <div class="box">
                       <div class="text">
                          <h3>Home Address:</h3>
-                         <p>&nbsp;Warangal, TS</p>
+                         <p>&nbsp;Hyderabad, India</p>
                       </div>
                    </div>
                    <div class="box">
                       <div class="text">
                          <h3>Mobile Phone:</h3>
-                         <p>&nbsp;+91-8331065200</p>
+                         <p>&nbsp;+91-6360591271</p>
                       </div>
                    </div>
                    <div class="box">
                       <div class="text">
                          <h3>Work Email:</h3>
-                         <p>&nbsp;kvsaimadhav@gmail.com</p>
+                         <p>&nbsp;saimadhav.kaza@gmail.com</p>
                       </div>
                    </div>
                 </div>
@@ -49,7 +49,7 @@ function Contact()
                <div class="formBx">
                 <form>
                  <label>First:</label>&#9;&#9;<input type="text" name="firstName" placeholder="First Name" defaultValue={"John"}/><br/>
-                 <label>Last:</label>&#9;&#9;;<input type="text" name="lastName" placeholder="Last Name" defaultValue={"Doe"}/><br/>
+                 <label>Last:</label>&#9;&#9;<input type="text" name="lastName" placeholder="Last Name" defaultValue={"Doe"}/><br/>
                  <label>Email:</label>&#9;&#9;<input type="Email" name="email" placeholder="Email Address" defaultValue={"john.doe@gmail.com"}/><br/>
                  <label>Contact:</label>&#9;&#9;&#9;<input type="text" name="phone" placeholder="Contact Number" defaultValue={"9999999999"}/><br/>
                  <label>Message:</label>&nbsp;<textarea placeholder="Your Message" defaultValue={"Hi, How are You ..."}/>
